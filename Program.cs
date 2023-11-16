d8fa1cb4832c205555eef391bb12278e2d050680
Szczepan Ćwikliński <sec@thinkcode.pl>
2023-11-16 20:06:20 +0100

    PublishAot + .net 8.0 target

--- Program.cs
+++ Program.cs
@@ -4,16 +4,17 @@ global using System.Runtime.InteropServices;
 global using System.Text;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateSlimBuilder(args);
 
 builder.Services.AddTransient<GeminiRequestHandler>();
 builder.WebHost.ConfigureKestrel(o =>
 {
+
     o.ListenAnyIP(1965, b =>
     {
         b.Protocols = HttpProtocols.None;
         b.UseHttps(Path.Combine(AppContext.BaseDirectory, "certificate.pfx"), "", o =>
-        {            
+        {
             o.SslProtocols = System.Security.Authentication.SslProtocols.Tls12 | System.Security.Authentication.SslProtocols.Tls13;
             o.AllowAnyClientCertificate();
         });
